2afc98a368558234037a6850f743f2b3bdba3dad
MayWonMbappe <3131499491@qq.com>
2026-06-20 08:26:00 +0800

    Delete js directory

--- js/app.js
+++
@@ -1,4 +0,0 @@
-import { loadState } from './state.js';
-import { startRouter } from './router.js';
-loadState();
-startRouter();
--- js/router.js
+++
@@ -1,8 +0,0 @@
-import { render } from './render.js';
-export function startRouter() {
-  window.addEventListener('hashchange', render);
-  if (!location.hash) location.hash = '#/';
-  render();
-}
-export function go(path) { location.hash = path.startsWith('#') ? path : `#${path}`; }
-export function routeParts() { return location.hash.replace(/^#\/?/, '').split('/').filter(Boolean); }
